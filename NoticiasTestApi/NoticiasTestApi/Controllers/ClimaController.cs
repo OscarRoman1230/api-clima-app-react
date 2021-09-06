748627cbb3ebc21ebed1935c7f40b3e8fb5c4acc
OscarRoman1230 <48228133+OscarRoman1230@users.noreply.github.com>
2021-09-06 16:18:12 -0500

    add project

---
+++ NoticiasTestApi/NoticiasTestApi/Controllers/ClimaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClimaController : ControllerBase
+    {
+        [HttpGet("{city_name}")]
+        public IActionResult Get(string city_name)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=7443e56f98f834a84f54830391c2b854";
+            WebRequest wrUrl = WebRequest.Create(url);
+            WebResponse wrpGet = wrUrl.GetResponse();
+            StreamReader sr = new StreamReader(wrpGet.GetResponseStream());
+            return Ok(sr.ReadToEnd().Trim());
+        }
+    }
+}
